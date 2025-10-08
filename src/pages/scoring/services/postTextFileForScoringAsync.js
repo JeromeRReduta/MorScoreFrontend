ff50415f32e581c1cebbd89560562a0441965318
JRReduta <jrreduta@yahoo.com>
2025-10-07 18:16:35 -0700

    Confirmed talking w/ API works - postTextFileForScoringAsync tested

--- src/pages/scoring/services/postTextFileForScoringAsync.js
+++ src/pages/scoring/services/postTextFileForScoringAsync.js
@@ -1,4 +1,6 @@
 export default async function postTextFileForScoringAsync({ algorithm, text }) {
+  const port = "http://localhost:3000";
+  const url = new URL(`${port}/get-your-morscore`);
   const request = {
     method: "POST",
     headers: {
@@ -10,7 +12,11 @@ export default async function postTextFileForScoringAsync({ algorithm, text }) {
     }),
   };
   console.log("request is", request);
-  const data = await fetch("/get-your-morscore", request);
 
-  return data;
+  const response = await fetch(url, request);
+  if (!response.ok) {
+    throw new Error("Error in request");
+  }
+  const content = await response.text();
+  return content.length > 0 ? JSON.parse(content) : {};
 }
